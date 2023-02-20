7aaf40dadfc9d0ef956e7ef3683a209445a13c8c
Lemorz56 <sebastian@msbrg.net>
2023-02-20 23:42:38 +0100

    wip settings page

---
+++ src/routes/my/settings/+layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  export let data: any;
+
+  const navigation: any[] = [
+    {
+      title: "Profile",
+      href: "/my/settings/profile",
+    },
+    {
+      title: "Account",
+      href: "/my/settings/account",
+    },
+    {
+      title: "Security",
+      href: "/my/settings/security",
+    },
+  ];
+</script>
+
+<div class="h-full w-full px-2">
+  <h3 class="text-2xl font-medium">Settings</h3>
+  <div class="divider" />
+</div>
+<div class="flex h-full w-full space-x-4">
+  <ul class="menu rounded-box w-56 bg-base-100 p-2">
+    {#each navigation as navItem}
+      <li>
+        <a
+          href={navItem.href}
+          class="font-medium {$page.url.pathname === navItem.href
+            ? 'active'
+            : ''}">{navItem.title}</a
+        >
+      </li>
+    {/each}
+  </ul>
+  <div class="w-full">
+    <slot />
+  </div>
+</div>
---
+++ src/routes/my/settings/profile/+page.svelte
@@ -0,0 +1,19 @@
+<div class="flex h-full w-full flex-col">
+  <form
+    action="?/updateProfile"
+    method="post"
+    class="flex w-full flex-col space-y-2"
+  >
+    <h3 class="text-2xl font-medium">Update Profile</h3>
+    <div class="form-control w-full max-w-lg">
+      <label for="avatar" class="label font-medium pb-1">
+        <span class="label-text">Profile Picture</span>
+      </label>
+      <label for="avatar" class="avatar w-32 rounded-full">
+        <div class="w-32 rounded-full">
+          <img src="https://loremflickr.com/80/80/human" alt="user avatar" />
+        </div>
+      </label>
+    </div>
+  </form>
+</div>
