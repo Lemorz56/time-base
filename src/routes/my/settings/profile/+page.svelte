141f04c2511df29c2cda51c32e89c678b2b54e0e
Lemorz56 <sebastian@msbrg.net>
2023-02-21 22:41:12 +0100

    wip settings page

--- src/routes/my/settings/profile/+page.svelte
+++ src/routes/my/settings/profile/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import { Icon, Pencil } from 'svelte-hero-icons'
+</script>
+
 <div class="flex h-full w-full flex-col">
   <form
     action="?/updateProfile"
@@ -9,11 +13,17 @@
       <label for="avatar" class="label font-medium pb-1">
         <span class="label-text">Profile Picture</span>
       </label>
-      <label for="avatar" class="avatar w-32 rounded-full">
+      <label for="avatar" class="avatar w-32 rounded-full hover:cursor-pointer">
+        <label for="avatar" class="absolute -bottom-0.5 -right-0.5 hover:cursor-pointer">
+          <span class="btn btn-circle btn-sm btn-secondary">
+            <Icon src={Pencil} class="w-4 h-4"/>
+          </span>
+        </label>
         <div class="w-32 rounded-full">
-          <img src="https://loremflickr.com/80/80/human" alt="user avatar" />
+          <img src="https://plchldr.co/i/500x250?text=CHANGE-ME" alt="user avatar" /> <!-- https://loremflickr.com/80/80/human -->
         </div>
       </label>
+      <input type="file" name="avatar" id="avatar" value="" accept="image/*" hidden />
     </div>
   </form>
 </div>
